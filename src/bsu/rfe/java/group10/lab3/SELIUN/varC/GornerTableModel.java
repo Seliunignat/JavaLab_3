a90ab22fb2a2af8765a8740bc8ff71198741580f
Ignat Seliun <seliunignat@gmail.com>
2020-11-06 10:01:42 +0300

    ADDED: MainFrame, Realised: GornerTableModel

---
+++ src/bsu/rfe/java/group10/lab3/SELIUN/varC/GornerTableModel.java
@@ -0,0 +1,69 @@
+package bsu.rfe.java.group10.lab3.SELIUN.varC;
+
+import javax.swing.table.AbstractTableModel;
+
+public class GornerTableModel extends AbstractTableModel
+{
+    private Double[] coefficients;
+    private Double from;
+    private Double to;
+    private Double step;
+
+    public Double getFrom() {
+        return from;
+    }
+    public Double getTo() {
+        return to;
+    }
+    public Double getStep() {
+        return step;
+    }
+
+    public GornerTableModel(Double from, Double to, Double step, Double[] coefficients)
+    {
+        this.from = from;
+        this.to = to;
+        this.step = step;
+        this.coefficients = coefficients;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 2;
+    }
+
+    @Override
+    public int getRowCount() {
+        return new Double(Math.ceil((to-from)/step)).intValue() + 1;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        // Вычислить значение X как НАЧАЛО_ОТРЕЗКА + ШАГ*НОМЕР_СТРОКИ
+        double x = from + step*rowIndex;
+        if (columnIndex==0) {
+            return x;
+        } else {
+            Double result;
+            // Вычисление значения в точке по схеме Горнера.
+            // Вспомнить 1-ый курс и реализовать
+            // ...
+            return result;
+        }
+    }
+
+    public Class<?> getColumnClass(int columnIndex) {
+        return Double.class;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        switch (column)
+        {
+            case 0:
+                return "Значение X";
+            default:
+                return "Значение многочлена";
+        }
+    }
+}
---
+++ src/bsu/rfe/java/group10/lab3/SELIUN/varC/MainFrame.java
@@ -0,0 +1,10 @@
+package bsu.rfe.java.group10.lab3.SELIUN.varC;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
